606e633140e0de92ab2dce43fd9f94a4c7c83d60
valed-dm <dmvaled@gmail.com>
2025-03-10 02:35:43 +0300

    'orders' API routes registered.

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from bistro.orders.api.views import MenuViewSet
+from bistro.orders.api.views import OrderViewSet
 from bistro.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
-router.register("users", UserViewSet)
+router.register(r"users", UserViewSet)
+router.register(r"menu", MenuViewSet)
+router.register(r"orders", OrderViewSet)
 
 
 app_name = "api"
