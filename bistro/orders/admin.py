7bb2d2f66abe6e0a26d5cf588a198e2ebdbf8e13
valed-dm <dmvaled@gmail.com>
2025-03-10 01:34:41 +0300

    'bistro' models registered in admin.py.

--- bistro/orders/admin.py
+++ bistro/orders/admin.py
@@ -1,1 +1,49 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Menu
+from .models import Order
+from .models import OrderItem
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+    list_filter = ("price",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "table_number",
+        "status",
+        "created_at",
+        "updated_at",
+        "total_price",
+    )
+    list_filter = ("status", "created_at", "updated_at")
+    search_fields = ("table_number",)
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    @admin.display(
+        description="Total Price",
+    )
+    def total_price(self, obj):
+        return obj.total_price
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "menu_item", "quantity", "total_price")
+    list_filter = ("order", "menu_item")
+    search_fields = ("order__id", "menu_item__name")
+
+    @admin.display(
+        description="Total Price",
+    )
+    def total_price(self, obj):
+        return obj.total_price
