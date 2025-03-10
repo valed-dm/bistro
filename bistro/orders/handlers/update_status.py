a6d6e940570fb47194fe7823a018bcd28a96cf3e
valed-dm <dmvaled@gmail.com>
2025-03-10 13:09:32 +0300

    update_status.py got docstrings.

--- bistro/orders/handlers/update_status.py
+++ bistro/orders/handlers/update_status.py
@@ -1,12 +1,24 @@
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpRequest
+from django.http import HttpResponse
 from django.shortcuts import redirect
 
 from bistro.orders.forms import UpdateStatusForm
 from bistro.orders.models import Order
 
 
-def handle_update_status(request):
+def handle_update_status(request: HttpRequest) -> HttpResponse:
+    """
+    Handles updating the status of an order based on the submitted form data.
+
+    Args:
+        request (HttpRequest): The HTTP request object containing form data.
+
+    Returns:
+        HttpResponse: Redirects to the manager view after attempting to update
+        the order status.
+    """
     form = UpdateStatusForm(request.POST)
     if form.is_valid():
         order_id = form.cleaned_data["order_id"]
