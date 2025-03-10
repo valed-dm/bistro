2aae6926f3c4c07b2fd718970af4bbf978a21846
valed-dm <dmvaled@gmail.com>
2025-03-10 13:08:24 +0300

    modals.py got docstrings.

--- bistro/orders/handlers/modals.py
+++ bistro/orders/handlers/modals.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from bistro.orders.forms import DeleteOrderForm
 from bistro.orders.forms import OrderForm
 from bistro.orders.forms import SearchOrderForm
@@ -5,7 +7,14 @@ from bistro.orders.forms import TotalPaidOrdersForm
 from bistro.orders.forms import UpdateStatusForm
 
 
-def get_modals():
+def get_modals() -> list[dict[str, Any]]:
+    """
+    Returns a list of modal configurations, each containing an ID, form, and title.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, where each dictionary represents
+        a modal configuration with keys 'id', 'form', and 'title'.
+    """
     return [
         {"id": "addOrderModal", "form": OrderForm(), "title": "Add Order"},
         {"id": "deleteOrderModal", "form": DeleteOrderForm(), "title": "Delete Order"},
